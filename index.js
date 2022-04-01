0ecacebe224a81472d1473a899f39621eb41fd62
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2022-04-01 12:18:10 +0530

    :sparkles: Add CLI
    Add CLI

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const server = require("./lib/server");
 const workers = require("./lib/workers");
+const cli = require("./lib/cli");
 
 const app = {};
 
 app.init = function () {
   server.init();
   workers.init();
+
+  setTimeout(function () {
+    cli.init();
+  }, 50);
 };
 app.init();
 module.exports = app;
